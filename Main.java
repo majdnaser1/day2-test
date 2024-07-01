f09a56bb59823b4189f2418048421874e7b6e46f
Majd Naser <110352036+majdnaser1@users.noreply.github.com>
2024-07-01 13:51:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,105 @@
+package org.example;
+
+import java.net.ServerSocket;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Java Basics
+
+        //java
+
+        /* 1.variables
+           2.Data Types
+           3.Scanner
+           4.Operations : Arithmetic , comparison , logical
+           5.Methods of String */
+
+
+
+        //variables : name , age , email
+        // 12name  1name  out System  first name   first_name firstName  name@   second_name
+
+        // Data Types : String , char , short , int , float , double , long , byte , boolean
+
+        // syntax : <Data Type> <var name> = <value > ;
+
+        // data type  var ;
+        // var=value;
+//
+//        String name = "Majd";
+//
+//        String last_name;
+////
+//        last_name="Naser";
+//
+//
+//        System.out.println(name+"  "+ last_name);
+//
+//
+//
+//        name="sara";
+//
+//
+//        System.out.println(name + last_name);
+//
+//       int number=30;
+//
+//       float degree_float=3.566778900000048765554438f;
+//
+//       double degree_double=4.8776555889999000988;
+//
+//       boolean power=true;
+//
+//       char score ='A';
+
+
+        Scanner input =new Scanner(System.in);
+
+//
+//        System.out.println("please enter your name:");
+//        String name=input.nextLine();
+//        System.out.println("Hello"+ " "+ name);
+//
+//        System.out.println("please enter your age");
+//
+//        int age;
+//
+//        age=input.nextInt();
+//
+//
+//        System.out.println();
+
+
+
+
+        // Arithmetic : +   -   *   %  /
+
+
+
+        String bootcamp=" JAVA Bootcamp ";
+
+
+        System.out.println(bootcamp.equals(" JAVA Bootcamp"));
+        System.out.println(bootcamp.equalsIgnoreCase(" java bootcamp"));
+        System.out.println(bootcamp.length());
+        System.out.println(bootcamp.trim());
+
+        System.out.println(bootcamp.length());
+        String test=bootcamp.trim();
+
+        System.out.println(test.length());
+
+
+        //
+
+
+
+
+
+
+
+
+    }
+}
